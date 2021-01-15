f0929ea85903d42047e4f57967cabffcd588ab78
mgahn0706 <mgahn0706@naver.com>
2021-01-15 15:20:06 +0900

    1

--- assets/js/main.js
+++ assets/js/main.js
@@ -259,4 +259,27 @@
 
 			});
 
+			var $menu = $('#menu'),
+				$menu_openers = $menu.children('ul').find('.opener2');
+
+			// Openers.
+				$menu_openers.each(function() {
+
+					var $this = $(this);
+
+					$this.on('click', function(event) {
+
+						// Prevent default.
+							event.preventDefault();
+
+						// Toggle.
+							$menu_openers.not($this).removeClass('active');
+							$this.toggleClass('active');
+
+						// Trigger resize (sidebar lock).
+							$window.triggerHandler('resize.sidebar-lock');
+
+					});
+
+				});
 })(jQuery);
