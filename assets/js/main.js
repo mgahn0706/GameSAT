497f989d84b4aade3a1e2de4be9a98d821bd4797
mgahn0706 <mgahn0706@naver.com>
2021-01-15 18:57:47 +0900

    test

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,8 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+
+
 (function($) {
 
 	var	$window = $(window),
@@ -259,27 +261,4 @@
 
 			});
 
-			var $menu = $('#menu'),
-				$menu_openers = $menu.children('ul').find('.opener2');
-
-			// Openers.
-				$menu_openers.each(function() {
-
-					var $this = $(this);
-
-					$this.on('click', function(event) {
-
-						// Prevent default.
-							event.preventDefault();
-
-						// Toggle.
-							$menu_openers.not($this).removeClass('active');
-							$this.toggleClass('active');
-
-						// Trigger resize (sidebar lock).
-							$window.triggerHandler('resize.sidebar-lock');
-
-					});
-
-				});
 })(jQuery);
